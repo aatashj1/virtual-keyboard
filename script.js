8a9bbfe4e581c6254e9cc3503f8f6265995c4804
aatashj1 <frnkxoxo69@gmail.com>
2023-04-30 14:32:56 +0300

    feat: add letters

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-console.log('hello, world!');
+const keyboardLayouts = {
+  english: [
+    "` 1 2 3 4 5 6 7 8 9 0 - =",
+    "q w e r t y u i o p [ ] \\",
+    "a s d f g h j k l ; '",
+    "z x c v b n m , . /"
+  ],
+  russian: [
+    "ё 1 2 3 4 5 6 7 8 9 0 - =",
+    "й ц у к е н г ш щ з х ъ \\",
+    "ф ы в а п р о л д ж э",
+    "я ч с м и т ь б ю ."
+  ]
+};
